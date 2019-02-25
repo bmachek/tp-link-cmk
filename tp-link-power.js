cab50f83ef60b4c4a0d28798958c4d55a5899676
Bastian Machek <bastian@machek.info>
2019-02-25 11:17:20 +0100

    initial commit

---
+++ tp-link-power.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+var host = process.argv[2]
+
+const { Client } = require('tplink-smarthome-api');
+const client = new Client();
+
+const plug = client.getDevice({'host': host}).then((device)=>{
+	device.getInfo().then(logPower);
+});
+
+
+function logPower(data)  {
+	var power = data.emeter.realtime.power;
+	var alias = data.sysInfo.alias;
+	alias = alias.replace(/ /, "_");
+	console.log("<<<tp-link-power>>>");
+	console.log(host + " \'" + alias  + "\' " + power);
+}
